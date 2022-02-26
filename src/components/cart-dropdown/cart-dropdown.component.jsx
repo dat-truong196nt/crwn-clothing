d8e2cf0c1e1fe384c1959e530aeae5578571b3c9
dat.truong <truongquocdat196nt@gmail.com>
2022-02-26 17:35:47 +0700

    feat: caching with selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
-import CartItem from "../cart-item/cart-item.component"
+import CartItem from "../cart-item/cart-item.component";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selector";
 
-const Cart = ({items}) => {
+const Cart = ({ items }) => {
 	return (
 		<div className="cart-dropdown">
-			{items && items.map(item => <CartItem key={item.id} {...item}/>)}
+			<div className="cart-items">
+				{items && items.map((item) => <CartItem key={item.id} {...item} />)}
+			</div>
 			<CustomButton>Go to checkout</CustomButton>
 		</div>
 	);
 };
 
-const mapStateToProp = ({cart: {items}}) =>({items})
+const mapStateToProp = (state) => ({
+	items: selectCartItems(state),
+});
 
 export default connect(mapStateToProp)(Cart);
--- src/redux/cart/cart.selector.js
+++ src/redux/cart/cart.selector.js
@@ -2,7 +2,7 @@ import { createSelector } from "selector";
 
 const selectCart = (state) => state.cart;
 
-const selectCartItems = createSelector(selectCart, (cart) => cart.items);
+export const selectCartItems = createSelector(selectCart, (cart) => cart.items);
 
 export const selectCartItemsCount = createSelector(selectCartItems, (items) =>
 	items.reduce((acc, item) => acc + item.quantity, 0)
